7d82849f9f8ec26f4a26f5ba87db838bd9bc269f
Mohit Singhal <singhalmohit011@gmail.com>
2024-05-14 22:50:50 +0530

    Removed pract folder

--- pract/src/ui/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Input = ({type, placeholder, value}) => {
-  return (
-    <input type={type} placeholder={placeholder} className='p-2 outline-none'/>
-  )
-}
-
-export default Input
